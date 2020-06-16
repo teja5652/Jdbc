267a0244b00a82175748e6f837619a71e3615481
teja.5652 <teja.sai5652@gmail.com>
2020-06-16 12:58:07 -0400

    first

---
+++ Employee.java
@@ -0,0 +1,28 @@
+  package com.package1;
+
+public class Employee {
+	
+	int empid;
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	String name;
+	
+	/*
+	 * @Override public String toString() {
+	 * 
+	 * return ("firstName" +getEmpid() );
+	 * 
+	 * }
+	 * 
+	 */
+	}
